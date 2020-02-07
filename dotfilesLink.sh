948b8b5f22597aa1bdde0d693cfafe0afa9436c9
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2020-02-08 00:39:29 +0900

    chore:update zsh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,5 +4,3 @@ ln -sf ~/dotfiles/gitconfig/.gitconfig_private ~/.gitconfig_private
 ln -sf ~/dotfiles/gitconfig/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.zpreztorc ~/.zprezto/runcoms/zpreztorc
-ln -sf ~/dotfiles/.zshrc ~/.zprezto/runcoms/zshrc
