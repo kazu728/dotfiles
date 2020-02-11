937af9b8de87e3183d2352a5ad1d9642e9e28ba6
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2020-02-11 14:33:33 +0900

    chore:add default module

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -4,3 +4,5 @@ ln -sf ~/dotfiles/gitconfig/.gitconfig_private ~/.gitconfig_private
 ln -sf ~/dotfiles/gitconfig/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/anyenv/nodenv/default-packages ~/.anyenv/envs/nodenv/default-packages
+ln -sf ~/dotfiles/anyenv/rbenv/default-gems ~/.anyenv/envs/rbenv/default-gems
