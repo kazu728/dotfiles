25ba313d5cea968dd2a829098da8f5bf12ad00cb
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2025-12-28 02:44:29 +0900

    removed unused commands

--- mac/nixpkgs/home.nix
+++ mac/nixpkgs/home.nix
@@ -8,35 +8,23 @@ in
   home.username = username;
   home.homeDirectory = lib.mkForce homeDirectory;
   home.packages = with pkgs; [
-    bottom
     bun
-    checkov
     gh
     ghq
-    helix
     mise
     nil
-    podman
     procs
     rustup
   ];
 
   home.stateVersion = "25.05";
-  home.file.".config/ghostty/config".source = ../ghostty/config;
-  home.file.".config/helix/config.toml".source = ../helix/config.toml;
-  home.file.".config/helix/themes/onedark_pro_night_flat.toml".source =
-    ../helix/themes/onedark_pro_night_flat.toml;
+  xdg.enable = true;
+  xdg.configFile."ghostty/config".source = ../ghostty/config;
   home.file.".claude/commands" = {
     source = ../claude/commands;
     recursive = true;
   };
-  home.file.".gitignore".text = ''
-    .DS_Store
-    node_modules
-    .envrc
-    .direnv
-  '';
-  
+
   nix.gc = {
     automatic = true;
     frequency = "monthly";
@@ -44,7 +32,6 @@ in
   };
 
   programs = {
-    bat.enable = true;
     eza.enable = true;
     home-manager.enable = true;
 
@@ -53,58 +40,48 @@ in
       delta.enable = true;
       userName = "Kazuki Matsuo";
       userEmail = "kazuki.matsuo.728@gmail.com";
+      ignores = [ ".DS_Store" "node_modules" ".envrc" ".direnv" ];
       extraConfig = {
-        core = {
-          editor = "nvim";
-          ignorecase = false;
-          excludesfile = "~/.gitignore";
-        };
-        color.ui = true;
-        diff.compactionHeuristic = true;
-        init.defaultBranch = "master";
-        merge.ff = false;
-        pull.rebase = true;
-        rerere = {
-          enabled = true;
-          autoUpdate = true;
-        };
         branch = {
           sort = "-committerdate";
         };
-        tag = {
-          sort = "-version:refname";
-          gpgsign = true;
+        color.ui = true;
+        commit.gpgsign = true;
+        core = {
+          editor = "nvim";
+          ignorecase = false;
         };
-        push = {
-          default = "current";
-          autoSetupRemote = true;
-        };
+        diff.compactionHeuristic = true;
         fetch = {
           prune = true;
           prunetags = true;
         };
+        gpg.format = "ssh";
         help = {
           autocorrect = "immediate";
+        };
+        init.defaultBranch = "master";
+        merge.ff = false;
+        pull.rebase = true;
+        push = {
+          default = "current";
+          autoSetupRemote = true;
         };
         rebase = {
           autosquash = true;
           autostash = true;
           updateRefs = true;
         };
-        url = { "https://github.com/".insteadOf = "git@github.com:"; };
-        commit.gpgsign = true;
-        gpg.format = "ssh";
-        user.signingKey = "${homeDirectory}/.ssh/id_github_rsa.pub";
-      };
-    };
-
-    lazygit = {
-      enable = true;
-      settings = {
-        git.paging = {
-          colorArg = "always";
-          pager = "delta --paging=never";
+        rerere = {
+          enabled = true;
+          autoUpdate = true;
         };
+        tag = {
+          sort = "-version:refname";
+          gpgsign = true;
+        };
+        url = { "https://github.com/".insteadOf = "git@github.com:"; };
+        user.signingKey = "${homeDirectory}/.ssh/id_github_rsa.pub";
       };
     };
 
@@ -133,7 +110,6 @@ in
         ls = "eza";
         ll = "ls -a";
         lla = "ls -la";
-        cat = "bat";
         ps = "procs";
         dust = "du";
         grep = "grep --color";
@@ -150,7 +126,6 @@ in
         dc = "docker container";
         doco = "docker compose";
         nix-direnv = "echo 'use flake' >> .envrc && direnv all";
-        "??" = "gh copilot suggest -t shell '$1'";
       };
 
       initContent = builtins.readFile ../zsh/.zshrc;
