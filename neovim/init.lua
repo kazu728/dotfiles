5bf9323e1a7e4e926856c923c90e0a1f0a116545
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2026-05-29 20:54:18 +0900

    configure fzf-lua grep and lsp references in neovim

--- neovim/init.lua
+++ neovim/init.lua
@@ -12,7 +12,12 @@ vim.keymap.set({ "n", "i" }, "<C-j>", "<Esc>")
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, { command = "checktime" })
 vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, { command = "silent! wall" })
 
-require("fzf-lua").setup({})
+require("fzf-lua").setup({
+  grep = {
+    hidden = true,
+    rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 --glob '!**/.git/**' -e",
+  },
+})
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<cr>", { desc = "Live grep" })
 vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Buffers" })
@@ -49,7 +54,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(args)
     local opts = { buffer = args.buf }
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "gr", function()
+      require("fzf-lua").lsp_references()
+    end, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
