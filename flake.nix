48e0e0638b546f514dcf2c04775bc2c58f790d36
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2026-06-02 20:54:11 +0900

    update Nix flake inputs to 26.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Darwin system configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hunk = {
@@ -12,7 +12,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     darwin = {
-      url = "github:lnl7/nix-darwin/nix-darwin-25.11";
+      url = "github:lnl7/nix-darwin/nix-darwin-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
