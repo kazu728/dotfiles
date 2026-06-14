e6a95152985dbc87ff5a5a12c06ba4b19df8574b
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2026-06-14 11:31:53 +0900

    configure yazi preview pane toggle keymap

--- nixpkgs/modules/yazi.nix
+++ nixpkgs/modules/yazi.nix
@@ -1,8 +1,25 @@
-_:
+{ pkgs, ... }:
 
 {
   programs.yazi = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      mgr = {
+        ratio = [ 0 1 2 ];
+      };
+    };
+    plugins = {
+      toggle-pane = pkgs.yaziPlugins.toggle-pane;
+    };
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = "T";
+          run = "plugin toggle-pane max-preview";
+          desc = "プレビューを最大化/元に戻す";
+        }
+      ];
+    };
   };
 }
