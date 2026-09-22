f77d71c0ef80867139e20d538d7f97cfd0a9132e
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2026-09-22 21:07:08 +0900

    replace finalAttrs with let binding in docker-sbx override

--- nix/packages/docker-sbx.nix
+++ nix/packages/docker-sbx.nix
@@ -1,10 +1,13 @@
 { docker-sbx, fetchurl }:
 
-docker-sbx.overrideAttrs (finalAttrs: {
+let
   version = "0.43.0";
+in
+docker-sbx.overrideAttrs {
+  inherit version;
 
   src = fetchurl {
-    url = "https://github.com/docker/sbx-releases/releases/download/v${finalAttrs.version}/DockerSandboxes-darwin.tar.gz";
+    url = "https://github.com/docker/sbx-releases/releases/download/v${version}/DockerSandboxes-darwin.tar.gz";
     hash = "sha256-6fybO66cVyilcm9JubNgkyhRnaV288mf2Ofl0TcAinw=";
   };
-})
+}
